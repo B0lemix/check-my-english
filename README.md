0c84949797d9b4560b4e725c09ef2297d79d810e
B0lemix <109000296+B0lemix@users.noreply.github.com>
2023-02-13 15:03:31 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Check my English AI!
+
+```
+
+```
+
+> 🧑‍🚀 **How it works** Delete this file. Have fun!
+
+![basics](https://user-images.githubusercontent.com/4677417/186188965-73453154-fdec-4d6b-9c34-cb35c248ae5b.png)
+
+
+## 🚀 Project Description
+
+
---
+++ src/components/Textarea.svelte
@@ -0,0 +1,30 @@
+<script>
+  import {isValidInput} from './store.js'
+  import {checkIsEnglish} from '../services/ia.js'
+  const handleChange= async (e) =>{
+    console.log(e.target.value);
+    const {value}=e.target
+    const isValid = value.length>0
+    if (!isValid){ 
+      isValidInput.set(false)
+      return
+  }
+
+    const isEnglish= await checkIsEnglish(value)
+    isValidInput.set(isEnglish)
+
+  }
+    
+</script>
+
+<textarea
+  on:input={handleChange}
+  class="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-gray-300 rounded-lg appearance-none focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent resize-none"
+  id="result"
+  placeholder="Put your text to check and fix..."
+  name="result"
+  rows="5"
+  cols="40"
+  
+>
+</textarea>
